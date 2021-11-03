da2d8985e2a27bb3140ce8e52c8649c92b4c8394
Valentin1311 <valent.flageul@gmail.com>
2021-11-03 01:45:29 +0100

    page stock

--- ProjectAWI/src/app/app.module.ts
+++ ProjectAWI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StockComponent } from './components/stock/stock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule,
