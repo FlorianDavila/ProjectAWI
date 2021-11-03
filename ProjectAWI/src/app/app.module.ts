50ab6206d851ce962ae495f38881fbc218f4de16
Valentin1311 <valent.flageul@gmail.com>
2021-11-03 14:39:48 +0100

    changes in stock

--- ProjectAWI/src/app/app.module.ts
+++ ProjectAWI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StockComponent } from './components/stock/stock.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockComponent
+    StockComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
