dc2556cf5aa5613abd3115293e0f8fb0f6c0a491
Florian DAVILA <florian.davila95@hotmail.fr>
2022-01-10 21:14:19 +0100

    icm

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -4,6 +4,8 @@ import {
   NgbSlideEvent,
   NgbSlideEventSource,
 } from '@ng-bootstrap/ng-bootstrap';
+import { Meal } from 'src/app/models/Meal';
+import { MealService } from 'src/app/services/meal.service';
 
 @Component({
   selector: 'app-carousel',
@@ -11,11 +13,11 @@ import {
   styleUrls: ['./carousel.component.css']
 }) 
 export class CarouselComponent { 
-  images = {
-    'assets/entree.jpg': ['Salade', 'download?meal=%7B%22id%22:%223MpKG7NklWZMvmzkrfK9%22,%22name%22:%22Salade%22,%22manager%22:%22Assistant%22,%22category%22:%22Entr%C3%A9e%22,%22nbGuests%22:%225%22,%22stageList%22:%5B%7B%22duration%22:%222%5C%22%22,%22ingredients%22:%5B%5D,%22name%22:%22Pr%C3%A9paration%22,%22description%22:%22Laver%20la%20salade%22%7D%5D,%22matS%22:null,%22matD%22:null%7D'],
-    'assets/plat.jpg': ['Boeuf bourgignon', 'download?meal=%7B%22id%22:%229HY3EKgvX37ttZY0xDhY%22,%22name%22:%22Boeuf%20bourgignon%22,%22manager%22:%22Chef%22,%22category%22:%22Entr%C3%A9e%22,%22nbGuests%22:%224%22,%22stageList%22:%5B%7B%22duration%22:%2290%5C%22%22,%22ingredients%22:%5B%5D,%22description%22:%22Faire%20cuire%20la%20viande%20%22,%22name%22:%22Cuisson%22%7D%5D,%22matS%22:null,%22matD%22:null%7D'],
-    'assets/dessert.jpg': ['Gaufres', 'download?meal=%7B%22id%22:%229hrstNFteIWyfi30Cljt%22,%22name%22:%22Gaufres%22,%22manager%22:%22Patissier%22,%22category%22:%22Dessert%22,%22nbGuests%22:%222%22,%22stageList%22:%5B%7B%22duration%22:%225%5C%22%22,%22name%22:%22Pr%C3%A9paration%22,%22ingredients%22:%5B%5D,%22description%22:%22Mettre%20la%20farine%20dans%20un%20saladier,%20y%20ajouter%20le%20sucre,%20les%20jaunes%20d%27%C5%93ufs%20et%20le%20beurre%20ramolli.%22%7D%5D,%22matS%22:null,%22matD%22:null%7D']
-  };
+
+  constructor(public mealService: MealService) { }
+
+  meals: Meal[] = [];
+  images: { [id: string]: Meal; } = {};
   paused = false;
   unpauseOnArrow = false;
   pauseOnIndicator = false;
@@ -24,6 +26,25 @@ export class CarouselComponent {
 
   @ViewChild('carousel', { static: true })
   carousel!: NgbCarousel; 
+
+  ngOnInit() {
+    this.mealService.getMealByName("Salade").subscribe(data => {
+      this.meals.push(data[0]);
+      this.images['assets/dessert.jpg'] = this.meals[0];
+    }); 
+    this.mealService.getMealByName("Boeuf bourgignon").subscribe(data => {
+      this.meals.push(data[0]);
+      this.images['assets/plat.jpg'] = this.meals[1];
+    }); 
+    this.mealService.getMealByName("Gaufres").subscribe(data => {
+      this.meals.push(data[0]);
+      this.images['assets/dessert.jpg'] = this.meals[2];
+    }); 
+  }
+  
+  helperPassData(meal: Meal) : string {
+    return JSON.stringify(meal);
+  }
 
   togglePaused() {
     if (this.paused) {
--- src/app/services/meal.service.ts
+++ src/app/services/meal.service.ts
@@ -34,6 +34,14 @@ export class MealService {
     this.mealsCollection.doc(meal.id).set(Object.assign({}, meal));
   }
 
+  getMealByName(name : String) : Observable<Meal[]>{
+    var ing = this.db.collection(this.path, ref => ref.where("name", "==", name));
+    return ing
+    .valueChanges({ idField: "id" }).pipe(
+    map(data => data.map(doc => this.jsonToMeal(doc))) 
+    );
+  }
+
   jsonToMeal(json: any) : Meal { 
     return new Meal( 
       json.id,
